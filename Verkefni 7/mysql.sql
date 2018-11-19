a0289a2e1fc82509afa0e8bbed0ee45c23691071
Storm011 <34028228+Storm011@users.noreply.github.com>
2018-11-19 12:04:26 +0000

    Delete mysql.sql

--- Verkefni 7/mysql.sql
+++
@@ -1,9 +0,0 @@
-create database 2608012360_verk7;
-
-CREATE TABLE 2608012360_verk7.users(
-	user varchar(32) not null,
-    pass varchar(32) not null,
-    nafn varchar(32) not null,
-    PRIMARY KEY (user)
-);
-insert into 2608012360_verk7.users(user,pass,nafn) value ('2608012360','123456','Sunna')
